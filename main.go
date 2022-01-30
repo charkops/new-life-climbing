a062239867680f982ca18367f33120577f831ff4
Charis Kopsacheilis <charis.kopsacheilis@gmail.com>
2022-01-30 16:57:23 +0200

    Create example route /ping

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Hi, climbing Greece!")
+	// What do we wanna do here ?
+	// Initialize DB
+	// Initialize routers/routes
+	// Serve
+	r := gin.Default()
+	r.Run()
 }
