0ae51372ffece6679d3c832f0c11ee226849b1ac
Charis Kopsacheilis <charis.kopsacheilis@gmail.com>
2022-03-18 15:19:21 +0200

    Init kinda with html templates

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Hi, climbing Greece!")
+	r := gin.Default()
+
+	r.LoadHTMLGlob("web/*")
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			"title": "Main website",
+		})
+	})
+
+	r.Run()
 }
