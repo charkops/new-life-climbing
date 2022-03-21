83a7490dd96a2eddf2edfddab73c4d3c0c7b156e
Charis Kopsacheilis <charis.kopsacheilis@gmail.com>
2022-03-21 15:18:17 +0200

    Initial multi template example

--- main.go
+++ main.go
@@ -3,16 +3,43 @@ package main
 import (
 	"net/http"
 
+	"github.com/gin-gonic/contrib/renders/multitemplate"
 	"github.com/gin-gonic/gin"
 )
+
+func makeTemplates() multitemplate.Render {
+	templates := multitemplate.New()
+
+	templates.AddFromFiles("index",
+		"web/base.html",
+		"web/index.html",
+		"web/header.html",
+		"web/footer.html",
+	)
+
+	templates.AddFromFiles("about",
+		"web/base.html",
+		"web/about.html",
+		"web/header.html",
+		"web/footer.html",
+	)
+
+	return templates
+}
 
 func main() {
 	r := gin.Default()
 
-	r.LoadHTMLGlob("web/*")
+	r.HTMLRender = makeTemplates()
 	r.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.html", gin.H{
-			"title": "Main website",
+		c.HTML(http.StatusOK, "index", gin.H{
+			"Title": "Main website",
+		})
+	})
+
+	r.GET("/about", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "about", gin.H{
+			"Title": "About",
 		})
 	})
 
